3f508dbcfab8c49d694eafd79be73009b2cd5be1
liljequv <liljequv@arcada.fi>
2024-01-08 11:37:20 +0200

    Lektion 1

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Car car = new Car("Tesla");
+        Car anotherCar = new Car("Fiat");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+        System.out.println(car.getName());
+        System.out.println(anotherCar.getName());
+        anotherCar.setName("BMW");
+        System.out.println(anotherCar.getName());
+
     }
 }
